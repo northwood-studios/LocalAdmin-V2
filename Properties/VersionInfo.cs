0f971291bba08c3adca2cae5482ef86c0ccea18e
iRebbok <irebbok@outlook.com>
2020-06-05 22:40:55 +0300

    include processing of file & assembly versions

--- Properties/VersionInfo.cs
+++ Properties/VersionInfo.cs
@@ -1,3 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyInformationalVersion(LocalAdmin.V2.Core.LocalAdmin.VersionString)]
+[assembly: AssemblyFileVersion(LocalAdmin.V2.Core.LocalAdmin.VersionString)]
+[assembly: AssemblyVersion(LocalAdmin.V2.Core.LocalAdmin.VersionString)]
