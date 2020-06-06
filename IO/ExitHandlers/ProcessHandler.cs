1675c5c4782a7129fd1e58b2bd5932ff9cd9a1de
petris <paprikapolishgamer@gmail.com>
2020-06-06 17:48:31 +0200

    Fix Process handler

--- IO/ExitHandlers/ProcessHandler.cs
+++ IO/ExitHandlers/ProcessHandler.cs
@@ -3,18 +3,20 @@ using System.Diagnostics;
 
 namespace LocalAdmin.V2.IO.ExitHandlers
 {
-	internal sealed class ProcessHandler : IExitHandler
-	{
-		public static readonly ProcessHandler Handler = new ProcessHandler();
+    internal sealed class ProcessHandler : IExitHandler
+    {
+        public static readonly ProcessHandler Handler = new ProcessHandler();
 
-		public void Setup()
-		{
-			Process.GetCurrentProcess().Exited += Exit;
-		}
+        public void Setup()
+        {
+            var process = Process.GetCurrentProcess();
+            process.EnableRaisingEvents = true;
+            process.Exited += Exit;
+        }
 
-		private static void Exit(object? sender, EventArgs e)
-		{
-			Core.LocalAdmin.Singleton.Exit(0);
-		}
-	}
+        private static void Exit(object? sender, EventArgs e)
+        {
+            Core.LocalAdmin.Singleton.Exit(0);
+        }
+    }
 }
