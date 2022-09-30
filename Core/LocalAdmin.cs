5d075fd7029d023c9b29468e712def5f915d3747
Łukasz Jurczyk <zabszk@protonmail.ch>
2022-09-30 20:33:21 +0200

    Version bump

--- Core/LocalAdmin.cs
+++ Core/LocalAdmin.cs
@@ -30,7 +30,7 @@ namespace LocalAdmin.V2.Core;
 
 public sealed class LocalAdmin : IDisposable
 {
-    public const string VersionString = "2.4.5";
+    public const string VersionString = "2.5.0";
     internal static LocalAdmin? Singleton;
     internal static ushort GamePort;
     internal static string? ConfigPath, LaLogsPath, GameLogsPath;
