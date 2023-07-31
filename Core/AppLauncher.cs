37475296646f41bf9b9e95d237fdadf5816ee1b3
ɅLΞX <123724383+ALEXWARELLC@users.noreply.github.com>
2023-07-31 10:21:25 +0800

    Added AppLauncher.cs.

---
+++ Core/AppLauncher.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LocalAdmin.V2.Core;
+
+//Feel free to suggest a better name.
+class AppLauncher
+{
+    public static bool DoesSteamVersionExist()
+    {
+        
+    }
+}
