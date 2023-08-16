b549e506df19d13c9da6a2a1d0164efca473fcee
ɅLΞX <123724383+ALEXWARELLC@users.noreply.github.com>
2023-08-16 13:07:39 +0800

    Reverse All Commits

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,4 +1,3 @@
-using System.IO;
 using System.Threading.Tasks;
 
 namespace LocalAdmin.V2.Core;
@@ -11,18 +10,10 @@ internal static class Program
             Utf8Json.Resolvers.GeneratedResolver.Instance,
             Utf8Json.Resolvers.BuiltinResolver.Instance
         );
-        if(File.Exists("startup.txt")){
-            while (true)
-            {
-                using var la = new LocalAdmin();
-                await la.Start(AutoStartupArguments.ReadStartupArgs());
-            }
-        }else{
-            while (true)
-            {
-                using var la = new LocalAdmin();
-                await la.Start(args);
-            }
+        while (true)
+        {
+            using var la = new LocalAdmin();
+            await la.Start(args);
         }
         // ReSharper disable once FunctionNeverReturns
     }
